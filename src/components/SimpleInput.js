af43efcb7267b3b9d6d23d316ead4ecf86a003b9
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-16 23:49:32 -0800

    added state/ref to get user input value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,38 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from 'react';
+
+const SimpleInput = props => {
+  const inputNameRef = useRef('');
+  const [enteredName, setEnteredName] = useState('');
+
+  const inputNameChangeHangler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+    // in case of using State
+    console.log('from State: ', enteredName);
+    setEnteredName('');
+
+    // in case of using Ref
+    const enteredValue = inputNameRef.current.value;
+    console.log('from Ref: ', enteredValue);
+    // inputNameRef.current.value = ''; => NOT IDEAL to manipulate DOM through React
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={inputNameRef}
+          type='text'
+          id='name'
+          onChange={inputNameChangeHangler}
+          value={enteredName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
